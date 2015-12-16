f31236da796dfd4c2ac907a73a952989a8c0d501
Nikhilesh Prasannakumar <nikhilesh.prasannakumar@gmail.com>
2015-12-16 13:37:09 +0530

    added uart echo example

---
+++ StarShipXP-Tiva/examples/ccs/launchpad-examples/005-uart-echo/Debug/uart_echo.pp
@@ -0,0 +1,27 @@
+# FIXED
+
+uart_echo.obj: ../uart_echo.c
+uart_echo.obj: C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+uart_echo.obj: C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+uart_echo.obj: C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+uart_echo.obj: C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+uart_echo.obj: C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+uart_echo.obj: C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+uart_echo.obj: C:/TexasInstruments/TivaWare_C_Series-2.1.1.71/inc/hw_memmap.h
+uart_echo.obj: C:/TexasInstruments/TivaWare_C_Series-2.1.1.71/driverlib/gpio.h
+uart_echo.obj: C:/TexasInstruments/TivaWare_C_Series-2.1.1.71/driverlib/pin_map.h
+uart_echo.obj: C:/TexasInstruments/TivaWare_C_Series-2.1.1.71/driverlib/sysctl.h
+uart_echo.obj: C:/TexasInstruments/TivaWare_C_Series-2.1.1.71/driverlib/uart.h
+
+../uart_echo.c: 
+C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+C:/TexasInstruments/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+C:/TexasInstruments/TivaWare_C_Series-2.1.1.71/inc/hw_memmap.h: 
+C:/TexasInstruments/TivaWare_C_Series-2.1.1.71/driverlib/gpio.h: 
+C:/TexasInstruments/TivaWare_C_Series-2.1.1.71/driverlib/pin_map.h: 
+C:/TexasInstruments/TivaWare_C_Series-2.1.1.71/driverlib/sysctl.h: 
+C:/TexasInstruments/TivaWare_C_Series-2.1.1.71/driverlib/uart.h: 
---
+++ StarShipXP-Tiva/examples/ccs/launchpad-examples/005-uart-echo/uart_echo.c
@@ -0,0 +1,30 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/uart.h"
+
+int main(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    GPIOPinConfigure(GPIO_PA0_U0RX);
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+
+    UARTConfigSetExpClk(UART0_BASE, SysCtlClockGet(), 115200,
+                            (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
+
+    while(1)
+    {
+        while(UARTCharsAvail(UART0_BASE))
+        {
+            UARTCharPut(UART0_BASE, UARTCharGet(UART0_BASE)+1);
+        }
+    }
+}
