708b4c8de7940ea568fee686f66861ac5d8cb8f6
Nikhilesh Prasannakumar <nikhilesh.prasannakumar@gmail.com>
2015-12-18 15:29:51 +0530

    added new demo

---
+++ StarShipXP-Tiva/examples/energia/starship-examples/sensor_demo/sensor_demo.ino
@@ -0,0 +1,76 @@
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+#include <Wire.h>
+#include <SPI.h>
+#include "DHT.h"
+#include <LiquidCrystal.h>
+#include <StarShipXP.h>
+
+#define RS PF_1
+#define EN PE_0
+#define D4 PE_4
+#define D5 PA_5
+#define D6 PE_3
+#define D7 PC_6
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
+
+#define DHTPIN PB_2     // what pin we're connected to
+
+#define DHTTYPE DHT11   // DHT 22  (AM2302)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+unsigned int currentValue=120, previousValue=120, timeBase = 1;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  lcd.begin(16,2);
+  dht.begin();
+  lcd.print(" Temp  Humidity");
+  SPI.setModule(2);
+  pinMode(GLCD_SEL1,OUTPUT);
+  digitalWrite(GLCD_SEL1,HIGH);
+  
+  Tft.begin(GLCD_SEL2,GLCD_DC,PF_0,GLCD_RESET);
+  Tft.TFTinit();
+}
+
+void loop() {
+  delay(100);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t) ) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C ");
+  
+  lcd.setCursor(1,1);
+  lcd.print(t);
+  
+  lcd.setCursor(8,1);
+  lcd.print(h);
+ 
+  int potValue = analogRead(POT);
+  currentValue = 235-(potValue/18); 
+  Tft.drawLine(timeBase-1, previousValue, timeBase, currentValue, WHITE);
+  previousValue = currentValue;
+  timeBase = timeBase+1;
+  
+  if(timeBase > 300)
+  {
+    timeBase = 1;
+    Tft.TFTinit();
+  }
+}
